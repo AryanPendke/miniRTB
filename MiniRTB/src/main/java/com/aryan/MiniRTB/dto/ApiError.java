2a4026d06b2ea47e62d4f8eaa4dc97bea2f8eb7e
AryanPendke <acppendke@gmail.com>
2026-06-30 21:03:07 +0530

    gitignore updated

---
+++ MiniRTB/src/main/java/com/aryan/MiniRTB/dto/ApiError.java
@@ -0,0 +1,13 @@
+package com.aryan.MiniRTB.dto;
+
+import java.time.LocalDateTime;
+
+public record ApiError(
+    LocalDateTime timestamp,
+    int status,
+    String error,
+    String message,
+    String path
+) {
+    
+}
